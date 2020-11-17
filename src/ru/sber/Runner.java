229fd99cc02871cfa274b1de41c0c418848ad8d3
VictoriyaBelikova <74599486+VictoriyaBelikova@users.noreply.github.com>
2020-11-17 14:37:43 +0300

    Add files via upload

--- src/ru/sber/Runner.java
+++ src/ru/sber/Runner.java
@@ -10,15 +10,15 @@ public class Runner {
     private static final int MAP_HEIGHT = 10;
 
     public static void main(String[] args) {
+        Generation previousGeneration = new Generation(MAP_WIDTH, MAP_HEIGHT, null);
+        Generation currentGeneration = new Generation(0, 0, previousGeneration);
+        Generation nextGeneration = new Generation(0, 0, previousGeneration);
 
         while (true) {
-            Generation previousGeneration = new Generation(MAP_WIDTH, MAP_HEIGHT, null);
-            Generation currentGeneration = new Generation(0, 0, previousGeneration);
-
             ViewGeneration.printGeneration(currentGeneration);
             previousGeneration.setGen(currentGeneration);
 
-            Generation nextGeneration = Model.calculateNextGeneration(currentGeneration);
+            Model.calculateNextGeneration(currentGeneration);
             currentGeneration.setGen(nextGeneration);
             ViewGeneration.printGeneration(currentGeneration);
         }
